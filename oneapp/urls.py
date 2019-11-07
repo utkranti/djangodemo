bf6acfd8559460434ff43db3d6e890b5ae0b3cfb
utkranti <utkrantijadhav@gmail.com>
2019-11-07 02:45:03 -0800

    first commit

---
+++ oneapp/urls.py
@@ -0,0 +1,10 @@
+from .views import EmpViewset
+from rest_framework import routers
+from django.urls import path, include
+
+router = routers.SimpleRouter()
+router.register(r'oneapp', EmpViewset)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
---
+++ twoapp/urls.py
@@ -0,0 +1,9 @@
+from .views import StudViewset
+from rest_framework import routers
+from django.urls import path, include
+
+router = routers.SimpleRouter()
+router.register(r'twoapp', StudViewset)
+urlpatterns = [
+    path('', include(router.urls))
+]
